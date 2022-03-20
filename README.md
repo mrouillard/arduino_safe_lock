157f08df11a81b4c64c5124ecb871bbba79302a8
Marc Rouillard <marc@rouillard.info>
2022-03-20 11:32:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# arduino_safe_lock
+# arduino_safe_lock 🔐
+
+### Note: heavily based on https://wokwi.com/arduino/libraries/demo/electronic-safe by Uri Shaked
+The main changes from the original are: 
+
+* LCD 16x2 wired using I²C
+* smaller membrane keypad (3x4, the original featured a 4x4)
+* messages in French
+
+1. Requirements
+
+    * arduino UNO
+    * LCD 16*2 with I2C backplate
+    * membrane keypad
+    * servo motor
+    * dupont female-female wires
+
+2. Wiring
---
+++ arduino_safe_lock.ino
@@ -0,0 +1,238 @@
+#include <Wire.h> // needed for I2C communications
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+#include "SafeState.h"
+#include "icons.h"
+
+/* Locking mechanism definitions */
+#define SERVO_PIN 6
+#define SERVO_LOCK_POS 20
+#define SERVO_UNLOCK_POS 90
+Servo lockServo;
+
+// init LCD display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Keypad setup
+const byte KEYPAD_ROWS = 4;
+const byte KEYPAD_COLS = 3;
+byte rowPins[KEYPAD_ROWS] = {5, 4, 3, 2};
+byte colPins[KEYPAD_COLS] = {A3, A2, A1};
+char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+
+/* SafeState stores the secret code in EEPROM */
+SafeState safeState;
+
+void lock()
+{
+  lockServo.write(SERVO_LOCK_POS);
+  safeState.lock();
+}
+
+void unlock()
+{
+  lockServo.write(SERVO_UNLOCK_POS);
+}
+
+void showStartupMessage()
+{
+  lcd.setCursor(4, 0);
+  lcd.print("Bonjour!");
+  delay(1000);
+
+  lcd.setCursor(0, 1);
+  String message = "ArduinoSafe v1.0";
+  for (byte i = 0; i < message.length(); i++)
+  {
+    lcd.print(message[i]);
+    delay(100);
+  }
+  delay(500);
+}
+
+String inputSecretCode()
+{
+  lcd.setCursor(5, 1);
+  lcd.print("[____]");
+  lcd.setCursor(6, 1);
+  String result = "";
+  while (result.length() < 4)
+  {
+    char key = keypad.getKey();
+    if (key >= '0' && key <= '9')
+    {
+      lcd.print('*');
+      result += key;
+    }
+  }
+  return result;
+}
+
+void showWaitScreen(int delayMillis)
+{
+  lcd.setCursor(2, 1);
+  lcd.print("[..........]");
+  lcd.setCursor(3, 1);
+  for (byte i = 0; i < 10; i++)
+  {
+    delay(delayMillis);
+    lcd.print("=");
+  }
+}
+
+bool setNewCode()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Entrer code:");
+  String newCode = inputSecretCode();
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Confirmer code");
+  String confirmCode = inputSecretCode();
+
+  if (newCode.equals(confirmCode))
+  {
+    safeState.setCode(newCode);
+    return true;
+  }
+  else
+  {
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("Code different");
+    lcd.setCursor(0, 1);
+    lcd.print("Coffre non fermé");
+    delay(2000);
+    return false;
+  }
+}
+
+void showUnlockMessage()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.write(ICON_UNLOCKED_CHAR);
+  lcd.setCursor(4, 0);
+  lcd.print("Déverrouillé!");
+  lcd.setCursor(15, 0);
+  lcd.write(ICON_UNLOCKED_CHAR);
+  delay(1000);
+}
+
+void safeUnlockedLogic()
+{
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.write(ICON_UNLOCKED_CHAR);
+  lcd.setCursor(2, 0);
+  lcd.print("# pour verrouiller");
+  lcd.setCursor(15, 0);
+  lcd.write(ICON_UNLOCKED_CHAR);
+
+  bool newCodeNeeded = true;
+
+  if (safeState.hasCode())
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("* = changer code");
+    newCodeNeeded = false;
+  }
+
+  auto key = keypad.getKey();
+  while (key != '*' && key != '#')
+  {
+    key = keypad.getKey();
+  }
+
+  bool readyToLock = true;
+  if (key == '*' || newCodeNeeded)
+  {
+    readyToLock = setNewCode();
+  }
+
+  if (readyToLock)
+  {
+    lcd.clear();
+    lcd.setCursor(5, 0);
+    lcd.write(ICON_UNLOCKED_CHAR);
+    lcd.print(" ");
+    lcd.write(ICON_RIGHT_ARROW);
+    lcd.print(" ");
+    lcd.write(ICON_LOCKED_CHAR);
+
+    safeState.lock();
+    lock();
+    showWaitScreen(100);
+  }
+}
+
+void safeLockedLogic()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.write(ICON_LOCKED_CHAR);
+  lcd.print("Coffre verrouillé");
+  lcd.write(ICON_LOCKED_CHAR);
+
+  String userCode = inputSecretCode();
+  bool unlockedSuccessfully = safeState.unlock(userCode);
+  showWaitScreen(200);
+
+  if (unlockedSuccessfully)
+  {
+    showUnlockMessage();
+    unlock();
+  }
+  else
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Accès refusé!");
+    showWaitScreen(1000);
+  }
+}
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  init_icons(lcd);
+
+  lockServo.attach(SERVO_PIN);
+
+  /* Make sure the physical lock is sync with the EEPROM state */
+  Serial.begin(115200);
+  if (safeState.locked())
+  {
+    lock();
+  }
+  else
+  {
+    unlock();
+  }
+
+  showStartupMessage();
+}
+
+void loop()
+{
+  // safeState.setCode("1234");
+  if (safeState.locked())
+  {
+    safeLockedLogic();
+  }
+  else
+  {
+    safeUnlockedLogic();
+  }
+}
